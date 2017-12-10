6c016fddd7148bcce62fc8898d55c74f1a076bc7
Andrew Haentjens <andrew@nextapps.be>
2017-12-10 13:11:43 +0100

    Initial project setup Added simple circleView

---
+++ LootPuzzle/LootPuzzle/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LootPuzzle
+//
+//  Created by Andrew Haentjens on 10/12/2017.
+//  Copyright © 2017 Andrew Haentjens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
