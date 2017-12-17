bee293d2be2563ec78f3763650929857c89b726d
Andrew Haentjens <andrew@nextapps.be>
2017-12-17 14:30:59 +0100

    created circle + an indicator rotating around it

--- LootPuzzle/LootPuzzle/GameScene.swift
+++ LootPuzzle/LootPuzzle/GameScene.swift
@@ -11,79 +11,108 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let oneCompleteTurn = CGFloat.pi * 2
+    let circleRadius: CGFloat = 100.0
+    let triangleLength: CGFloat = 20.0
+    let margin: CGFloat = 16.0
     
     override func didMove(to view: SKView) {
+        // Set Layout
+        setLayout()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // create main circle
+        let lock = createCircle()
+        addChild(lock)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let invisibleParent = SKSpriteNode()
+        invisibleParent.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(invisibleParent)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        // create indicator (basically the triangle)
+        let indicator = createEquilateralTriangle()
+        indicator.zRotation = CGFloat.pi / 2 // set original rotation so that the corner points inwards towards the center of the circle
+        indicator.position.x = circleRadius + margin // set the offet to center of circle minus the radius and a margin
+        invisibleParent.addChild(indicator)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        runRotationFor(node: invisibleParent, duration: 2)
+        
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+}
+
+// MARK: private functions
+
+extension GameScene {
+    
+    // MARK: - General
+    
+    private func setLayout() {
+        self.backgroundColor = SKColor.white
+    }
+    
+    // MARK: - Create figures
+    
+    /**
+        Creates a simple circle. Orange with a black border.
+     */
+    private func createCircle() -> SKShapeNode {
+        
+        let circle = SKShapeNode(circleOfRadius: circleRadius)
+        
+        circle.position = CGPoint(x: frame.midX, y: frame.midY)
+        circle.strokeColor = .black
+        circle.glowWidth = 1.0
+        circle.fillColor = .orange
+        
+        return circle
+    }
+    
+    /**
+        Creates a triangle with equal sides. Orange with a black border.
+     */
+    private func createEquilateralTriangle() -> SKShapeNode {
+        
+        // create an equal side triangle
+        let cSideSqrd = (triangleLength * triangleLength) - ((triangleLength / 2) * (triangleLength / 2)) // C^2 - A^2
+        let cSide = cSideSqrd.squareRoot()
+        
+        var points = [
+            CGPoint(x: triangleLength / 2.0, y: -cSide / 2.0),   // right under point
+            CGPoint(x: -triangleLength / 2.0, y: -cSide / 2.0),  // left under point
+            CGPoint(x: 0.0, y: cSide / 2.0),                              // top point
+            CGPoint(x: triangleLength / 2.0, y: -cSide / 2.0)    // right under point again
+        ]
+        
+        let triangle = SKShapeNode(points: &points, count: points.count)
+        
+        triangle.fillColor = .red
+        triangle.strokeColor = .black
+        triangle.glowWidth = 1.0
+        
+        return triangle
+        
+    }
+    
+    // MARK: - Animations
+    
+    /**
+        Create the rotating animation
+     */
+    private func runRotationFor(node: SKNode, isInfinite: Bool = true, duration: TimeInterval = 0.3) {
+        
+        let rotateAction = SKAction.rotate(byAngle: -oneCompleteTurn, duration: duration)
+        
+        if !isInfinite {
+            node.run(rotateAction)
+            return
+        }
+        
+        let repeatedRotateAction = SKAction.repeatForever(rotateAction)
+        node.run(repeatedRotateAction)
+        
     }
 }
